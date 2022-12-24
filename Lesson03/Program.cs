2f23ed7103b56a59f245357a9b6b9b7a31518974
NAG <MrAbhorrent@yandex.ru>
2022-12-24 15:00:00 +0500

    Added files

---
+++ Lesson03/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson03
+{
+    class Program
+    {
+        private class myPoint 
+        {
+            private int x;
+            private int y;
+            private int z;
+
+            public myPoint(int _x, int _y, int _z)
+            {
+                this.x = _x;
+                this.y = _y;
+                this.z = _z;
+            }
+
+            public double distance(myPoint anotherPoint)
+            {
+                double distance = 0.0;
+                distance = Math.Sqrt(Math.Pow((anotherPoint.x - this.x), 2) + Math.Pow((anotherPoint.y - this.y), 2) + Math.Pow((anotherPoint.z - this.z), 2));
+                return distance;
+            }
+
+            public override String ToString()
+            {
+                return String.Format("({0, 3}, {1, 3}, {2, 3})", this.x, this.y, this.z);
+            }
+        }
+
+        static private bool checkFiveDigit(int _number)
+        {
+            bool check = false;
+            if (_number > 9999 || _number < 100000)
+            {
+                check = true;
+            }
+            return check;
+        }
+
+        static private bool checkPalindrom(int _number)
+        {
+            bool isPalindrom = false;
+            int localNumber = _number;
+            int rightNumber, leftNumber;
+            int count = 5; // TODO: надо определить разрядность числа
+            int digit = 10;
+            while (count > 1)
+            {
+                leftNumber = (int)(localNumber / Math.Pow(digit, count - 1));
+                rightNumber = (int)(localNumber % Math.Pow(digit, 1));
+                if (leftNumber == rightNumber)
+                {
+                    isPalindrom = true;
+                }
+                else
+                {
+                    isPalindrom = false;
+                }
+                int number1 = (int)(leftNumber * Math.Pow(digit, count - 1));
+                int number2 = (int)(rightNumber * Math.Pow(digit, 1 - 1));
+                localNumber = (int)((int)(localNumber / Math.Pow(digit, 1)) % Math.Pow(digit, count - 1 - 1));
+                count -= 2;
+            }
+
+            return isPalindrom;
+        }
+
+        static private void printQubeTable(int N)
+        {
+            StringBuilder strBuild = new StringBuilder();
+            strBuild.Append(N + " -> ");
+            for (int i = 1; i <= N; i++)
+            {
+                strBuild.Append(Math.Pow(i, 3));
+                if (i != N)
+                {
+                    strBuild.Append(", ");
+                }                
+            }
+            Console.WriteLine(strBuild.ToString());
+            strBuild.Clear();
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+             * Задача 19
+             * Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+             * 14212 -> нет
+             * 12821 -> да
+             * 23432 -> да
+             */
+            Console.WriteLine("\nЗадача 19");
+            Console.WriteLine("---------");
+            int[] array = { 14212, 12821, 23432 };
+            foreach (int item in array)
+            {
+                if (checkPalindrom(item))
+                {
+                    Console.WriteLine("{0} - да", item);
+                }
+                else
+                {
+                    Console.WriteLine("{0} - нет", item);
+                }
+            }
+
+            Console.WriteLine("=======================================================================");
+            /* Задача 21
+            * Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+            * A (3,6,8); B (2,1,-7), -> 15.84
+            * A (7,-5, 0); B (1,-1,9) -> 11.53
+            */
+            Console.WriteLine("\nЗадача 21");
+            Console.WriteLine("---------");
+            myPoint pointA = new myPoint(3, 6,  8);
+            myPoint pointB = new myPoint(2, 1, -7);
+            Console.WriteLine("A {0} B {1} -> {2:f2}", pointA.ToString(), pointB.ToString(), pointA.distance(pointB));
+            pointA = new myPoint(7, -5, 0);
+            pointB = new myPoint(1, -1, 9);
+            Console.WriteLine("A {0} B {1} -> {2:f2}", pointA.ToString(), pointB.ToString(), pointA.distance(pointB));
+            Console.WriteLine("=======================================================================");
+            /* Задача 23
+            * Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+            * 3 -> 1, 8, 27
+            * 5 -> 1, 8, 27, 64, 125             * 
+            */
+            Console.WriteLine("\nЗадача 23");
+            Console.WriteLine("---------");
+            printQubeTable(3);
+            printQubeTable(5);
+
+
+            Console.ReadKey();
+        }
+    }
+}
